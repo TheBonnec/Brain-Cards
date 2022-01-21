a80e00ff68f27bb500eac05a4c2c01264e3350b5
TheBonnec <thomaslebonnec@icloud.com>
2022-01-21 19:16:10 +0100

    Initial Commit

---
+++ Shared/Brain_CardsApp.swift
@@ -0,0 +1,20 @@
+//
+//  Brain_CardsApp.swift
+//  Shared
+//
+//  Created by Thomas Le Bonnec on 21/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Brain_CardsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
